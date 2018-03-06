6a4489b5ec1f3d58bb9029829001feb75ee9672d
Stuart Breingan <sbreingan@googlemail.com>
2018-03-06 17:35:01 +0000

    Update shell script for running java program to read from twitter

--- start_kafka.sh
+++ start_kafka.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
 docker run -d -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=localhost --env ADVERTISED_PORT=9092 --name kafka spotify/kafka
+mvn -f kafkatwitter clean
+
+mvn -f kafkatwitter package
+
+java -jar kafkatwitter/target/kafka-twitter-1.0-SNAPSHOT-jar-with-dependencies.jar
