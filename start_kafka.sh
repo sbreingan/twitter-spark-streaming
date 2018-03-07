a7d447fefe13ba6cf9dabc9fa4b0d7240246b169
Stuart Breingan <sbreingan@googlemail.com>
2018-03-07 09:50:13 +0000

    Added CLI arguments for passing down twitter search keywords.

--- start_kafka.sh
+++ start_kafka.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-docker run -d -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=localhost --env ADVERTISED_PORT=9092 --name kafka spotify/kafka
+if [ ! "$(docker ps -q -f name=kafka_twitter)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=kafka_twitter)" ]; then
+        # cleanup
+        docker rm kafka_twitter
+    fi
+    # run your container
+    docker run -d -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=localhost --env ADVERTISED_PORT=9092 --name kafka_twitter spotify/kafka
+fi
+
 mvn -f kafkatwitter clean
 
 mvn -f kafkatwitter package
 
-java -jar kafkatwitter/target/kafka-twitter-1.0-SNAPSHOT-jar-with-dependencies.jar
+echo Twitter search terms: "$@" 
+
+java -jar kafkatwitter/target/kafka-twitter-1.0-SNAPSHOT-jar-with-dependencies.jar "$@"
