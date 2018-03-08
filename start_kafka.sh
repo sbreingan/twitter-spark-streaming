912075de2945a7102557022c52676ae40cbadebc
Stuart Breingan <sbreingan@googlemail.com>
2018-03-08 09:49:59 +0000

    updated script to reflect new folder structure

--- start_kafka.sh
+++ start_kafka.sh
@@ -9,10 +9,10 @@ if [ ! "$(docker ps -q -f name=kafka_twitter)" ]; then
     docker run -d -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=localhost --env ADVERTISED_PORT=9092 --name kafka_twitter spotify/kafka
 fi
 
-mvn -f kafkatwitter clean
+mvn -f kafka_twitter clean
 
-mvn -f kafkatwitter package
+mvn -f kafka_twitter package
 
 echo Twitter search terms: "$@" 
 
-java -jar kafkatwitter/target/kafka-twitter-1.0-SNAPSHOT-jar-with-dependencies.jar "$@"
+java -jar kafka_twitter/target/kafka-twitter-1.0-SNAPSHOT-jar-with-dependencies.jar "$@"
